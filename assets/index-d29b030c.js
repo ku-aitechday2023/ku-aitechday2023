7a8a1add22b8f439ab725ed3dfb6cb639c123f5e
hypnotes <hyp0notes@gmail.com>
2023-11-10 00:11:36 +0900

    Add files via upload

---
+++ assets/index-d29b030c.js
@@ -0,0 +1,11 @@
+import{s as r,B as t}from"./index-cf3f3e6c.js";const a=r(t)`
+  display: flex;
+  margin-top: 1px;
+  align-items: center;
+  color: ${({theme:o})=>o.palette.primary.main};
+  &:hover {
+    color: ${({theme:o})=>o.palette.secondary.light};
+    cursor: pointer;
+    transition: color 0.2s ease-in-out;
+  }
+`;export{a as L};
